8efb19f54259e21d643b5d4d3e32e60c971081ee
rahul-hasan25 <rahulhaasan2022@gmail.com>
2026-01-16 02:35:12 +0600

    blogs and search features

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render
 from blogs.models import Category, Blog
+from assignments.models import About
 
 
 def home(request):
     featured_post = Blog.objects.filter(is_featured=True, status='Published').order_by('updated_at')
     posts         = Blog.objects.filter(is_featured=False, status='Published')
     
+    # Fetch about us
+    try:
+        about = About.objects.get()
+    except:
+        about = None
+    
     context = {
         'featured_post': featured_post,
         'posts'        : posts,
+        'about'        : about,
     }
     
     return render(request, 'home.html', context)
--- blogs/views.py
+++ blogs/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Blog, Category
+from django.db.models import Q
 
 # Create your views here.
 def posts_by_category(request, category_id):
@@ -18,3 +19,37 @@ def posts_by_category(request, category_id):
     }
     
     return render(request, 'posts_by_category.html', context)
+
+def blogs(request, slug):
+    single_blog = get_object_or_404(Blog, slug=slug, status='Published')
+    # if request.method == 'POST':
+    #     comment = Comment()
+    #     comment.user = request.user
+    #     comment.blog = single_blog
+    #     comment.comment = request.POST['comment']
+    #     comment.save()
+    #     return HttpResponseRedirect(request.path_info)
+
+    # # Comments
+    # comments = Comment.objects.filter(blog=single_blog)
+    # comment_count = comments.count()
+    
+    context = {
+        'single_blog': single_blog,
+        # 'comments': comments,
+        # 'comment_count': comment_count,
+    }
+    
+    return render(request, 'blogs.html', context)
+
+# Search View
+def search(request):
+    keyword = request.GET.get('keyword')
+    
+    blogs = Blog.objects.filter(Q(title__icontains=keyword) | Q(short_description__icontains=keyword) | Q(blog_body__icontains=keyword), status='Published')
+  
+    context = {
+        'blogs': blogs,
+        'keyword': keyword,
+    }
+    return render(request, 'search.html', context)
