c09fe2a8969ead8ce9d19d0b456b04ee65b2728a
rahul-hasan25 <rahulhaasan2022@gmail.com>
2026-01-18 16:26:54 +0600

    final edit

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('category/', include('blogs.urls')),
     path('blogs/<slug:slug>/', BlogsView.blogs, name='blogs'),
     # Search endpoint
-    path('blogs/search/', BlogsView.search, name='search'),
+    path('search/', BlogsView.search, name='search'),
     
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
